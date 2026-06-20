5f5e171169ecb4a97a3b55de069a910a73d195b1
sanskritehe <sanskritisharmaa14@gmail.com>
2026-06-20 22:36:19 +0530

    feat: add app/graphql/schema.py for KAN-24

---
+++ app/graphql/schema.py
@@ -0,0 +1,75 @@
+import strawberry
+from app.graphql.resolvers import resolve_appointment_by_id, resolve_microsoft_repos, resolve_appointment_record
+from typing import Optional
+
+@strawberry.type
+class Appointment:
+    id: int
+    user: str
+    time: str
+    status: str
+
+@strawberry.type
+class AppointmentStorage:
+    id: int
+    user: str
+    time: str
+    status: str
+
+@strawberry.type
+class Repository:
+    id: int
+    name: str
+    html_url: str = strawberry.field(name="html_url")
+    description: str
+    language: str
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def appointmentById(self, id: int) -> Optional[Appointment]:
+        appointment = resolve_appointment_by_id(id=id)
+        return Appointment(
+            id=appointment["id"],
+            user=appointment["user"],
+            time=appointment["time"],
+            status=appointment["status"]
+        )
+    
+    @strawberry.field
+    def appointment(self, id: int) -> Optional[Appointment]:
+        appointment = resolve_appointment_by_id(id=id)
+        return Appointment(
+            id=appointment["id"],
+            user=appointment["user"],
+            time=appointment["time"],
+            status=appointment["status"]
+        )
+    
+    @strawberry.field
+    def appointment_record(self, id: int) -> Optional[AppointmentStorage]:
+        appointment = resolve_appointment_record(id=id)
+        if not appointment:
+            return None
+        return AppointmentStorage(
+            id=appointment.id,
+            user=appointment.user,
+            time=appointment.time,
+            status=appointment.status
+        )
+    
+    @strawberry.field
+    def microsoftRepos(self) -> list[Repository]:
+        repos = resolve_microsoft_repos()
+        return [
+            Repository(
+                id=repo["id"],
+                name=repo["name"],
+                html_url=repo["html_url"],
+                description=repo["description"],
+                language=repo["language"]
+            )
+            for repo in repos
+        ]
+
+schema = strawberry.Schema(query=Query)
