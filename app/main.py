2084bfa7862939a5735717f46a5224600daea2a2
sanskritehe <sanskritisharmaa14@gmail.com>
2026-06-25 20:00:11 +0530

    feat: add app/main.py for KAN-28

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes import appointments
+from app.database import SessionLocal
 
-app = FastAPI(title="REST API Gateway")
+app = FastAPI()
 
 app.include_router(appointments.router)
