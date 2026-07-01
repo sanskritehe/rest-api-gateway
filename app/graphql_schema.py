04b08e020b6cff8bb3b52f78d59c517f7e2f570c
sanskritehe <sanskritisharmaa14@gmail.com>
2026-07-01 16:25:50 +0530

    feat: add app/graphql_schema.py for KAN-31

--- app/graphql_schema.py
+++ app/graphql_schema.py
@@ -1,57 +1,30 @@
 import strawberry
 from typing import Optional
-from app.database import SessionLocal
-from app.models import AppointmentDB
 from app.services.booking_service import (
     get_appointment_by_id as get_appointment_by_id_service,
 )
 
 
 @strawberry.type
-class AppointmentStorage:
-    id: int
-    user: str
-    time: str
-    status: str
-
-
-@strawberry.type
 class Appointment:
     id: int
-    user: str
-    time: str
+    patientName: str
+    doctorName: str
+    date: str
     status: str
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def appointment_record(self, id: int) -> Optional[AppointmentStorage]:
-        db = SessionLocal()
-        try:
-            appointment = db.query(AppointmentDB).filter(AppointmentDB.id == id).first()  # type: ignore
-            if appointment:
-                return AppointmentStorage(
-                    id=appointment.id,
-                    user=appointment.user,
-                    time=appointment.time,
-                    status=appointment.status,
-                )
-            return None
-        finally:
-            db.close()
-
-    @strawberry.field
     def appointment(self, id: int) -> Optional[Appointment]:
         appointment = get_appointment_by_id_service(appointment_id=id)
         if not appointment:
-            return None
+            raise Exception("Appointment not found")
         return Appointment(
             id=appointment["id"],
-            user=appointment["user"],
-            time=appointment["time"],
+            patientName=appointment["patientName"],
+            doctorName=appointment["doctorName"],
+            date=appointment["date"],
             status=appointment["status"],
         )
-
-
-schema = strawberry.Schema(query=Query)
